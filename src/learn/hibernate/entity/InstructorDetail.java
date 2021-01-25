1823ffc0b49eb2916dfde8aebf5b75f36481107e
milad <milad.jlali@gmail.com>
2021-01-25 10:29:37 +0330

    Create instructorDetail Class

---
+++ src/learn/hibernate/entity/InstructorDetail.java
@@ -0,0 +1,32 @@
+package learn.hibernate.entity;
+
+import javax.annotation.Generated;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "instructor")
+public class InstructorDetail {
+	
+	
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name = "first_name")
+	private String firstname;
+	
+	@Column(name = "last_name")
+	private String lastname;
+	
+	@Column(name = "email")
+	private String email;
+	
+	
+
+}
